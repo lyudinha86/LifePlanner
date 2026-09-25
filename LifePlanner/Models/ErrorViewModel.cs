1afa968438768dbc71fb073d6434b654c138595d
lyudinha86 <lyudinha712@gmail.com>
2026-09-25 20:29:52 +0100

    Add project files.

---
+++ LifePlanner/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LifePlanner.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
